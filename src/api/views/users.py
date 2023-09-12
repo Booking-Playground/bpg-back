765083ce36261cde68807c625b428caaef1adbc1
Kirill Sobolev <sblvkr@gmail.com>
2023-09-12 12:10:43 +0300

    fix

--- src/api/views/users.py
+++ src/api/views/users.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from djoser.views import UserViewSet
+from drf_spectacular.utils import extend_schema
 
 from api.serializers.playground import UserReadSerializer
 
@@ -7,6 +8,7 @@ from api.serializers.playground import UserReadSerializer
 User = get_user_model()
 
 
+@extend_schema(tags=["Users"])
 class CustomUserViewSet(UserViewSet):
     queryset = User.objects.all()
     serializer_class = UserReadSerializer
