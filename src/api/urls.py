5cd49a6713d3a55d2b7453d59ecac22a067374f2
Лев Андреев <leoohard@yandex.ru>
2023-09-15 21:25:05 +0300

    add from api.views.booking import SettingBookingView, CreateBookingView

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework import routers
 from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
-from api.views.booking import SettingBookingView
+from api.views.booking import SettingBookingView, CreateBookingView
 from api.views.playground import (
     CoveringViewSet,
     SportViewSet,
